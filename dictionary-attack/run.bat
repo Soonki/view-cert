3e2aec83a3a3fd866bcbb5f39f5e00af154bc222
Soonki Ji <soonki.ji@gmail.com>
2018-03-05 21:00:42 -0500

    First commit.

--- dictionary-attack/run.bat
+++ dictionary-attack/run.bat
@@ -1,4 +1,4 @@
 @del output.txt > NUL 2>&1
-call mvnw -q exec:java -Dexec.mainClass=com.demo.Main -Dexec.args=
+call mvnw -q clean compile exec:java -Dexec.mainClass=com.demo.Main -Dexec.args=
 echo ---- output.txt ----
 type output.txt
